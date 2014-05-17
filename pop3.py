8f48018823e001fda989630ca2a1124456a64d67
ReFruity <istandon8@gmail.com>
2014-05-17 21:01:12 +0600

    base64 parsing

--- pop3.py
+++ pop3.py
@@ -1,7 +1,11 @@
 import socket
 import base64
+import re
+import sys
 
 class msglist:
+    msgs = []
+
     def __init__(self, str):
         lines = str.splitlines()
         self.num = len(lines)-2
@@ -9,11 +13,27 @@ class msglist:
         for i in lines[1:-1]:
             self.size.append(i[2:])
 
-class msgheader:
-    def __init__(self, str):
-        lines = str.splitlines()
+    class msgheader:
+        def __init__(self, str):
+            self.lines = re.split(r"\r\n(?=^[a-zA-Z0-9-_]+:)", str, flags=re.MULTILINE)
 
-def receive(sock, encoding = "UTF8"):
+        def get(self, field):
+            field = field.lower()
+            for i in self.lines:
+                if(i.lower().startswith(field)):
+                    result = re.sub(r"=[?](.+?)[?][bB][?](.+?)[?]=", b64repl, i)
+                    result = re.sub(r"\r\n\s*", "", result)
+                    return result
+            return "'" + field + "' information not found"
+
+def b64repl(match):
+    encoding = match.group(1)
+    code = match.group(2)
+    binary = base64.standard_b64decode(code)
+    result = binary.decode(encoding)
+    return result
+
+def receive(sock, encoding = "koi8-r"):
     result = ""
     while True:
         buffer = sock.recv(512)
@@ -22,12 +42,26 @@ def receive(sock, encoding = "UTF8"):
     return result
 
 if __name__ == "__main__":
+    usage = "Usage: pop3 username password"
+
     server = "pop.mail.ru"
     port = 110
 
     user = "pop3mailtester@mail.ru"
     passw = "testermail3pop"
-    debug = True
+    debug = False
+
+    sys.argv.pop(0)
+    if len(sys.argv) == 2:
+        user = sys.argv[0]
+        passw = sys.argv[1]
+    elif sys.argv:
+        print(usage)
+        exit()
+
+    print("Server:", server)
+    print("Port:", port)
+    print("Checking mail for", user)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     s.settimeout(5)
@@ -46,15 +80,18 @@ if __name__ == "__main__":
 
     s.send("LIST\n".encode())
     ans = receive(s)
-    # ans += s.recv(512).decode()
     if debug: print(ans)
 
     ml = msglist(ans)
+    # ml.num = 30
 
     for i in range(1, ml.num+1):
         s.send(("TOP " + str(i) + " 0\n").encode())
         ans = receive(s)
         if debug: print(ans)
-        mes = msgheader(ans)
+        ml.msgs.append(msglist.msgheader(ans))
 
-    # s.send((input()+'\n').encode())
+    for i in range(len(ml.msgs)):
+        print(i+1, ml.msgs[i].get("From:"))
+        print(" ", ml.msgs[i].get("Subject:"))
+        print("  Size: ", ml.size[i])
